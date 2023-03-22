286bdb2f004e2b1444a79fa8125d1f35ebb44225
Ferneko <fcgraciano@gmail.com>
2023-03-22 16:14:19 -0300

    Add project files.

---
+++ WebApp_Tarde/Controllers/UsuariosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Tarde.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
