4a08e2b99fbc87aec4c1e8901461c1e69a4df48d
Ferneko <fcgraciano@gmail.com>
2023-03-29 14:28:02 -0300

    Criação da tela de lista de clientes

---
+++ WebApp_Tarde/Controllers/ClientesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Tarde.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+    }
+}
