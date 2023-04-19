a902d43dd41476c2f1481429ae07b01a2274d530
Ferneko <fcgraciano@gmail.com>
2023-04-19 14:31:23 -0300

    Lista de Clientes com Model

--- WebApp_Tarde/Controllers/ClientesController.cs
+++ WebApp_Tarde/Controllers/ClientesController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_Tarde.Models;
 
 namespace WebApp_Tarde.Controllers
 {
     public class ClientesController : Controller
     {
+        public static List<ClientesViewModel> db = new List<ClientesViewModel>();
         public IActionResult Lista()
         {
-            return View();
+            ClientesViewModel c1 = new ClientesViewModel();
+            c1.Id = 1;
+            c1.Telefone = "991340447";
+            c1.Nome = "Fernando";
+
+            ClientesViewModel c2 = new ClientesViewModel();
+            c2.Id = 2;
+            c2.Nome = "Joao Garcia";
+            c2.Telefone = "16789432456";
+
+            db.Add(c1);
+            db.Add(c2);
+            return View(db);
         }
 
         public IActionResult Cadastro()
---
+++ WebApp_Tarde/Models/ClientesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp_Tarde.Models
+{
+    public class ClientesViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}
